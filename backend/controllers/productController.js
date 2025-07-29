29d697806abd12b05d6bc5ee657af9fa3d6d2d0e
SopnilSwapon <sopnilswapon1@gmail.com>
2025-07-30 02:49:13 +0600

    implement get method

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,19 @@
-export const getAllProducts = async (req, res) => {
-    
+import { sql } from "../config/db.js"
+
+export const getProducts = async (req, res) => {
+    try {
+    const products =    await sql`
+        SELECT *
+        FROM products
+        ORDER BY created_at DESC
+        `
+        console.log("fetched data", products)
+        res.status(200).json({success: true, data: products})
+    } catch (error) {
+        console.log(error)
+    }
 }
+export const getProduct = async (req, res) => {}
 export const createProduct = async (req, res) => {}
+export const updateProduct = async (req, res) => {};
+export const deleteProduct = async (req, res) => {}
